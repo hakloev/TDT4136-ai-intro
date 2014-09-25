07f413557b2619985ab10f9ea93d1572fae753a3
Håkon Ødegård Løvdal <me@hakloev.no>
2014-09-25 16:32:38 +0200

    started with astar.py

---
+++ astar.py
@@ -0,0 +1,14 @@
+# we will use python3
+
+class Astar():
+    
+    def __init__(self): 
+        print("init astar")
+
+class Node():
+
+    def __init__(self):
+        print("init node")
+
+if __name__ == "__main__":
+    a = Astar()
