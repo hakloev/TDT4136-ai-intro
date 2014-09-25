326a3bb42e11d92ed355ef9e11987dcca2f78eee
Håkon Ødegård Løvdal <me@hakloev.no>
2014-09-25 16:35:58 +0200

    inited

--- astar.py
+++ astar.py
@@ -1,4 +1,7 @@
-# we will use python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# We will implement the A*-algorithm with python3
 
 class Astar():
     
@@ -7,8 +10,17 @@ class Astar():
 
 class Node():
 
+    nodeName = None   
+
     def __init__(self):
         print("init node")
+        self.nodeName = "node1"
+
+    def name(self):
+        print(self.nodeName)
+        
 
 if __name__ == "__main__":
     a = Astar()
+    n = Node()
+    n.name()
