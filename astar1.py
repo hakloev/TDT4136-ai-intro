6b7c5e2d01d5929c2ddf4ffc115ac1278a393d81
Bergalerga <berg@dhcp-005177.wlan.ntnu.no>
2014-09-30 11:31:27 +0200

    ICANHASCOMMENTPLZ

--- astar1.py
+++ astar1.py
@@ -9,6 +9,12 @@ import heapq, sys
 class AStar(object):
         
     def __init__(self, debug=False):
+
+        """
+        Initializes the Astar object, which consists of all of the logic for performing a search using the 
+        A-star Algorithm
+        """
+
         self.debug = debug
         self.opened = []
         heapq.heapify(self.opened)
@@ -22,6 +28,9 @@ class AStar(object):
         self.agendaLoop()
 
     def agendaLoop(self):
+        """
+        The main loop of the algorithm.
+        """
         heapq.heappush(self.opened, (self.startNode.f, self.startNode))
         while len(self.opened):
             node = heapq.heappop(self.opened)[1]
@@ -40,12 +49,21 @@ class AStar(object):
                         heapq.heappush(self.opened, (adjNode.f, adjNode))
            
     def updateNode(self, adjNode, node):
+        """
+        Method used by the main loop, to update a node with a new set of values for the f, g and h functions.
+        @param node. The current node being worked on
+        @param adjNode. The node being updated 
+        """
         adjNode.g = node.g
         adjNode.h = self.heuristicMD(adjNode)
         adjNode.parent = node
         adjNode.f = adjNode.h + adjNode.g
 
     def readBoard(self):
+        """
+        Reads in a board from a text file, and stores each tile on the board as a node object. The nodes are 
+        stored in a matrix, which is used to represent the board.
+        """
         try:
             board = open('boards/board-%s.txt' % (sys.argv[1]) , 'r')
         except IndexError:
@@ -76,6 +94,10 @@ class AStar(object):
             self.nodes.append(listLine)
         
     def displayPath(self):
+        """
+        Starts from the end node, and traverses through the linked list using the parent attribute. Used 
+        to reconstruct the path from end to beginning.
+        """
         node = self.endNode
         while node.parent is not self.startNode:
             node = node.parent
@@ -86,10 +108,20 @@ class AStar(object):
             print()
 
     def getNode(self, x, y):    
-        #return self.nodes[(x * self.gridWidth) + y]
+        """
+        Gets the node at position x, y
+        @returns Node. The node found
+        @param x. The X coordinate
+        @param y. The Y coordinate
+        """
         return self.nodes[x][y]
 
     def getAdjacentNodes(self, node):
+        """
+        Finds and returns the nodes adjacent to a node as a list.
+        @returns List. list of nodes
+        @param node. Current node being searched
+        """
         nodes = []
         if node.x < self.gridHeight - 1:
             nodes.append(self.getNode(node.x + 1, node.y))
@@ -102,11 +134,24 @@ class AStar(object):
         return nodes
     
     def heuristicMD(self, node):
+        """
+        heuristic function being used. Calculates the manhattan distance between node and end.
+        @param node. The current node being searched
+        @returns Integer. The manhattan distance.
+        """
         return abs(node.x - self.endNode.x) + abs(node.y - self.endNode.y)
 
 class Node(object):
 
     def __init__(self, char, x, y, walkable):
+        """
+        Initializes a node object. This is used to represent a tile on the game board, as well as 
+        store information about the the g, h and f functions.
+        @param char. Character used to represent the tile on the board
+        @param x. The x coordinate
+        @param y. The y coordinate
+        @param walkable. Boolean value representing wether the tile is walkable
+        """
         self.char = char
         self.walkable = walkable
         self.x = x
